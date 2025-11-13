1b829fc849818f8044868215416a9b00d9151a13
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-11-13 20:06:15 -0300

    `Updated Routes.jsx with new imports and routes`

--- ARQATLAS-NEW/src/Routes.jsx
+++ ARQATLAS-NEW/src/Routes.jsx
@@ -1,23 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-<<<<<<< Updated upstream
-=======
 import Mapa from "./pages/Mapa";
 import ObraDetalhes from "./pages/ObraDetalhes";
 import Galeria from "./pages/Galeria";
->>>>>>> Stashed changes
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< Updated upstream
-=======
         <Route path="/galeria" element={<Galeria />} />
         <Route path="/mapa" element={<Mapa />} />
         <Route path="/obra/:id" element={<ObraDetalhes />} />
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
